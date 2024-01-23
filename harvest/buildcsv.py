8e521285e2c53b99394bcfc903242c4030c123d2
Justin Coyne <jcoyne@justincoyne.com>
2024-01-23 14:24:49 -0600

    Extract data

---
+++ harvest/buildcsv.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+directory = '_posts/'
+delimeter = '---'
+
+with open('data.csv', 'w', newline='') as out:
+  writer = csv.writer(out)
+  for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    print(filename)
+    delimeter_count = 0
+    title = ''
+    description = ''
+    with open(directory + filename, 'r') as input_file:
+      for line in input_file:
+        if line.startswith('title: '):
+          _, title = line.rsplit('title: ')
+        if delimeter_count > 1:
+          description += line
+        if line == '---\n':
+          delimeter_count += 1    
+    writer.writerow([title.rstrip(), description.rstrip()])
+        
+      
